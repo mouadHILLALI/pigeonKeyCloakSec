3e5394f43203342c8b64b66f507fad2b0b198b1c
mouadHILLALI <mauadtoto001@gmail.com>
2024-12-18 15:06:35 +0100

    PS-13 configured the cors

--- src/main/java/com/PigeonSkyRace/PigeonSkyRace/security/Impl/SecurityConfig.java
+++ src/main/java/com/PigeonSkyRace/PigeonSkyRace/security/Impl/SecurityConfig.java
@@ -1,16 +1,21 @@
 package com.PigeonSkyRace.PigeonSkyRace.security.Impl;
 
 import lombok.RequiredArgsConstructor;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -37,4 +42,16 @@ public class SecurityConfig {
                    .httpBasic(httpBasic -> httpBasic.authenticationEntryPoint(customAuthenticationEntryPoint))
         .build();
    }
+
+   @Bean
+   public CorsConfigurationSource corsFilter(){
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("https://localhost:8443"));
+        configuration.setAllowedMethods(Arrays.asList("GET" , "POST" , "DELETE" , "PUT" , "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Content-Type" , "Accept" , "Authorization"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+   } 
 }
