e40c85f36aa79d81eeaf23847027106c6e244b9b
mouadHILLALI <mauadtoto001@gmail.com>
2024-12-27 16:00:40 +0100

    commit

--- src/main/java/com/PigeonSkyRace/PigeonSkyRace/security/Impl/SecurityConfig.java
+++ src/main/java/com/PigeonSkyRace/PigeonSkyRace/security/Impl/SecurityConfig.java
@@ -11,7 +11,6 @@ import java.util.stream.Collectors;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.GrantedAuthority;
@@ -23,6 +22,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 
 @Configuration
 @EnableWebSecurity
